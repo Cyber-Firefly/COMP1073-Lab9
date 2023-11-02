1cf9924a51451e77d0d9578c506c0b7ec2f0f04f
John.Gu <john.gu@yahoo.com>
2023-11-02 19:57:58 -0400

    First commit

--- js/objects.js
+++ js/objects.js
@@ -1,8 +1,28 @@
-const output = document.getElementById('output');
+const output = document.getElementById("output");
 
 /* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), 
 then open this page in a browser and enter 'person' in the console */
-
+var person = {
+  name: {
+    firstName: "John",
+    lastName: "Gu",
+  },
+  age: 31,
+  gender: "Male",
+  address: {
+    streetNum: 1,
+    streetName: "Georgian Drive",
+    city: "Barrie",
+  },
+  interests: ["basketball", "coding", "cooking"],
+  greeting: function () {
+    alert(`Hello! My name is ${this.name.firstName} ${this.name.lastName}`);
+  },
+  bio: function () {
+    return `The interest of ${this.name.firstName} ${this.name.lastName}, of age ${this.age}, of gender ${this.gender} are ${this.interests}.`;
+  },
+};
+console.log(person);
 /* STEP 2a: Add other data items to the above object, like name, age, gender, address, and interests */
 
 /* STEP 2b: Enter the following directly into the console:
@@ -18,8 +38,8 @@ called "greeting" that creates an alert dialog, then type person.greeting() into
 /* STEP 4: Add a more complicated function to the object that describes the person, their age, gender, 
     and interests in a string that is output to an alert dialog */
 
-// The object above is called an 'object literal' - in that we build each part of the object manually, 
-// rather than instatiating it from a class (which we will cover later). 
+// The object above is called an 'object literal' - in that we build each part of the object manually,
+// rather than instantiating it from a class (which we will cover later).
 // Objects can be easier to work with than arrays, as you can name each element
 
 /* STEP 5a: Dot syntax allows you to access the parts of an object - the first part is the namespace, 
@@ -31,19 +51,21 @@ the string, instead of the alert(). */
 
 /* STEP 6a: An object can even contain another object - update object person above to include a name object. 
 Note how this breaks the greeting and bio functions - update the greeting function to use this sub-namespace */
-
+output.textContent = person.bio();
 /* STEP 6b: Now fix the bio() function to use the sub-namespace of 'name' as well */
 
 /* STEP 7: You can also use bracket notation, just like an array - 
 objects that are built this way are often referred to as associative arrays */
 
 /* STEP 8: You can also set members of an object - try changing the person's name, and age */
-
+person.name.firstName = "Shaochen";
 
 /* STEP 9a: It it also possible to add new members to an object - include eye color - and type person.eyes in the console */
-
+person.eyes = "black";
 /* STEP 9b: Add a new function called 'goodbye' to the person object and try it by typing person.goodbye into the console */
-
+person.goodbye = function () {
+  alert(`This is ${this.name.firstName} signing off!`);
+};
 /* STEP 10: We can also dynamically set both names and values for objects… */
 
 /* STEP 11a: 'this' is very useful - it allows us to refer to the object in question, specifically. 
@@ -52,10 +74,22 @@ each with a name, and a function that outputs the name */
 
 /* STEP 11b: Call up both person1.hello() and person2.hello() 
 in the console to see how 'this' is specific to each object */
+var person1 = {
+  name: "Person1",
+  greeting: function () {
+    alert(`Hello! My name is ${this.name}`);
+  },
+};
 
+var person2 = {
+  name: "Person2",
+  greeting: function () {
+    alert(`Hello! My name is ${this.name}`);
+  },
+};
 // You have already been using objects all along:
-let myString = 'Words, words, words.'; // this creates an instance of the class String
-const elem = document.getElementById('output'); // this accesses the Document object, which is created (instantiated) when the browser opens the HTML page
+let myString = "Words, words, words."; // this creates an instance of the class String
+const elem = document.getElementById("output"); // this accesses the Document object, which is created (instantiated) when the browser opens the HTML page
 
 // Next, open up oop.html and we will delve a bit deeper into JavaScript objects.
 
--- js/oop.js
+++ js/oop.js
@@ -1,16 +1,35 @@
-const output = document.getElementById('output');
-const output2 = document.getElementById('output2');
+const output = document.getElementById("output");
+const output2 = document.getElementById("output2");
 
 /* STEP 1a: Create a new object using a regular function */
-
+function createNewPerson(name) {
+  var obj = {};
+  obj.name = name;
+  obj.greeting = function () {
+    alert(`Hello! My name is ${this.name}`);
+  };
+  return obj;
+}
 /* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - 
 rewrite the above function, without returning anything. Capitalize the name of the function. */
-
+function Person(firstName, lastName, age, gender, interests) {
+  this.name = { firstName: firstName, lastName: lastName };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+  this.greeting = function () {
+    alert(`Hello! My name is ${this.name}`);
+  };
+}
 /* STEP 2b: Use the console to create a couple of different people, using the 'new' keyword, 
 and again invoking the .greeting() method for each person */
-
+var p1 = new Person("P1", "LP1", 35, "Female", [
+  "Swimming",
+  "Jogging",
+  "Dancing",
+]);
 /* STEP 3a: Build the complete constructor for the object Person (comment out the above function first).
  Include name (first and last), age, gender, interests, bio (function), and greeting (function). */
 
@@ -22,14 +41,19 @@ and again invoking the .greeting() method for each person */
 // person1.bio()
 
 /* STEP 4a: Alternatively, you can use the Object() constructor to create an object. eg. car*/
-
+var car = new Object();
 /* STEP 4b: Once 'car' is created, add various properties and methods… */
-
+car.brand = "Tesla";
+car.color = "White";
+car.price = 100;
+car.fun = function () {
+  alert(`${car.brand} ${car.color} ${car.price}`);
+};
 /* STEP 4c: Change some of the properties of 'car' in the console, then invoke the car.fun() function */
 
 /* STEP 5a: Yet another approach is to use the create() method. 
 Let's see how the above car object can be used to create another object */
-
+var anotherCar = Object.create(car);
 /* STEP 5b: Output to the paragraph anotherCar.brand - you will see that it has retained the properties of the original object. */
 
 // That's it! Now on to Lab 3...
@@ -44,7 +68,24 @@ Let's see how the above car object can be used to create another object */
     single, double, or triple patty, 
     Pickles, hot peppers, olives etc.
 */
-// add some extra if you want
-// Make sure that the function allows for the output of a sentence that describes the hamburger, too
 
+function Burger(bun, veggie, cheese, sauce, protein, stack, toppings) {
+  this.bun = bun;
+  this.veggie = veggie;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.protein = protein;
+  this.stack = stack;
+  this.toppings = toppings;
+
+  this.describe = function () {
+    output2.textContent = `The burger has a ${bun} bun, with ${veggie} as vegetable, topped with ${cheese} cheese. There are ${stack} ${protein} patties with ${sauce} sauce and the toppings are ${toppings}`;
+  };
+}
+// add some extra if you want
+var burger1 = new Burger("white", "Lettuce", "Cheddar", "BBQ", "Beef", 2, [
+  "pickles, onions, mushrooms",
+]);
+// Make sure that the function allows for the output of a sentence that describes the hamburger, too
+burger1.describe();
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
